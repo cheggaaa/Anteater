bca9641b548ac8721db94f07cc2066558b260cc9
FB_929 <fb_929@fb-929.name>
2012-09-05 15:12:43 +0400

    fix scripts/anteater_dump.sh

--- scripts/anteater_dump.sh
+++ scripts/anteater_dump.sh
@@ -7,20 +7,22 @@
 #########################################
 
 ### Vars
-DB_DIR="$1"
-BACKUPS_DIR="${2:-/opt/BACKUP/anteater}"
+BACKUPS_DIR="${1:-/opt/BACKUP/anteater}"
+USER="${2:-at}"
+GROUP="$USER"
 DATE=`date +%Y%m%d-%H%M`
 DUMP_DIR="$BACKUPS_DIR/${DATE}_dump"
 
 ### Check
-if [ -z "$DB_DIR" ] || [ ! -d "$DB_DIR" ]; then
-	cat <<EOF
-Usage: $0 path/to/anteater/db/dir [path/to/backups/dir]
+case $1 in
+	help|h|-h|--help)
+		cat <<EOF
+Usage: $0 [path/to/backups/dir] [anteater_user]
 EOF
-	exit 1
-fi
+		exit 1
+		;;
+esac
 
 ### Backup
-install -o root -m 0750 -d $BACKUPS_DIR $DUMP_DIR &&
-cp -rp $DB_DIR/file.index $DUMP_DIR/ &&
-cp -rp $DB_DIR/file.data.* $DUMP_DIR/
+echo "install -o $USER -g $GROUP -m 0750 -d $BACKUPS_DIR $DUMP_DIR &&"
+echo "aecommand dump $DUMP_DIR"
