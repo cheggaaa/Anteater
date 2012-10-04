3adc2180dfd188112eb2dd4d5572d784cda7eca8
Sergey Cherepanov <cheggaaa@gmail.com>
2012-10-04 14:10:47 +0400

    Md5 & copy

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -1,3 +1,19 @@
+/*
+  Copyright 2012 Sergey Cherepanov (https://github.com/cheggaaa)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 package storage
 
 import (
@@ -12,6 +28,7 @@ import (
 	"dump"
 	"fmt"
 	"stats"
+	"aelog"
 )
 
 const (
@@ -300,4 +317,18 @@ func (s *Storage) GetStats() *stats.Stats {
 	}
 		
 	return s.Stats
+}
+
+
+func (s *Storage) CheckMD5() (result map[string]bool) {
+	result = make(map[string]bool)
+	
+	for n, _ := range s.Index.Files {
+		f, ok := s.Get(n)
+		if ok {
+			aelog.Debugf("Checking md5 for %s\n", n)
+			result[n] = f.CheckMd5()
+		}
+	}
+	return
 }
